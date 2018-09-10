7c583755b9fa21991ba571e9df9868119278cdbe
t-nakane <je_pense_donc_je_suis_honor@yahoo.co.jp>
2018-09-10 20:24:20 +0900

    my first commit

---
+++ hangman.py
@@ -0,0 +1,41 @@
+# hangman
+
+def hangman(word):
+    wrong = 0
+    stages = ["______      ",
+              "|     |     ",
+              "|     |     ",
+              "|     O     ",
+              "|    /|\\    ",
+              "|    / \\    ",
+              "|___________"]
+    rletters = list(word)
+    board = ["_"] * len(word)
+    win = False
+    print("Welcome to Hangman!")
+    
+    while wrong < len(stages):
+        print()
+        msg = "Guess one charactor : "
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = "$"
+        else:
+            wrong += 1
+        
+        print(" ".join(board))
+        print("\n".join(stages[0:wrong]))
+
+        if "_" not in board:
+            print("\nYou win!")
+            print(" ".join(board))
+            win = True
+            break
+    
+    if not win:
+        print("You lose!")
+        print("It's the correct answer is {}".format(word))
+
+hangman("cat")
